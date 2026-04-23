e8f2caf65ae789f47c4f353beb663e59e23839a8
Hamza Arif <83561668+hamzaarif727@users.noreply.github.com>
2026-04-24 01:16:41 +0500

    Add files via upload

---
+++ ArrFuncIntToBin.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class ArrFuncIntToBin {
+    static int[] IntToBin(int num) {
+        int n = num;
+        int count = 0;
+
+        while (n > 0) {                   //figuring out the number of bits num needs in binary
+            count = count + 1;
+            n = n / 2;
+        }
+
+        int[] binArr = new int[count];
+        int index = count-1;
+
+        while (num > 0) {
+            int remainder = num % 2;
+            binArr[index] = remainder;
+            num = num / 2;
+            index = index - 1;
+
+        }
+
+        return binArr;
+    }
+
+    public static void  main(String[] args){
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Enter an Integer: ");
+        int integer = read.nextInt();
+
+        int[] output = IntToBin(integer);
+
+        System.out.print("The binary is: ");
+        for(int i=0; i < output.length;i++){
+            System.out.print(output[i]);
+        }
+    }
+}
