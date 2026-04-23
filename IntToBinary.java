b99fe75910968a12441e5d3100b5c37fba9bc871
Hamza Arif <83561668+hamzaarif727@users.noreply.github.com>
2026-04-24 00:24:20 +0500

    Add files via upload

---
+++ IntToBinary.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class IntToBinary {
+    public static void main(String[] args){
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Enter an Integer for conversion: ");
+        int integer = read.nextInt();
+
+        String binary = "";
+
+        while(integer > 0){
+            int remainder = integer % 2;
+            binary = remainder + binary;
+            integer = integer / 2;
+        }
+
+        System.out.println("The binary is " + binary );
+
+    }
+}
