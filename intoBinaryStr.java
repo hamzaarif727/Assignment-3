6b96e717ab1e93cff567614e0d90a1f639301ed8
Hamza Arif <83561668+hamzaarif727@users.noreply.github.com>
2026-04-24 06:48:43 +0500

    Add files via upload

---
+++ intoBinaryStr.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class intoBinaryStr {
+    static String binaryString(int num){
+         String bin = "";
+
+         while(num>0) {
+
+             int remainder = num % 2;
+             bin = remainder + bin;
+             num = num / 2;
+         }
+         return bin;
+    }
+
+    public static void  main(String[] args){
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Enter an Integer number: ");
+        int x = read.nextInt();
+
+        String binStrResult = binaryString(x);
+        System.out.println("The Binary is: "+ binStrResult);
+    }
+}
